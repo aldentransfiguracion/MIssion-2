35be3f5667aa6b9176f4418993f8134dfb17f9d8
alden <alden@ALDENTRANSFB1A6>
2024-01-24 12:24:35 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Mission2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        
+        //Prompt and collect input from user
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        string input = Console.ReadLine();
+        int numrolls = int.Parse(input);
+
+        //Print results
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1 % of the total number of rolls");
+        Console.WriteLine("Total number of rolls = " + numrolls);
+        
+        //Use method in RollDice class to print results
+        RollDice rd = new RollDice();
+        int[] rollResults = rd.Rolls(numrolls);
+        
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = rollResults[i - 2] * 100 / numrolls;
+            Console.WriteLine($"{i}: {new string('*', percentage)}");
+        }
+
+        Console.WriteLine("Thank you for using the dice throwing simulator!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class RollDice
+    {
+        //Random variable 
+        private static Random r = new Random();
+
+        //Method returns nothing. Takes in integer named roll
+        public int[] Rolls(int roll)
+        {
+            //Create array of integers named combos that is 11 "segments long"
+            int[] combos = new int[11];
+
+            //For every numnber in between 1 and the integer passed
+            for (int i = 0; i < roll ; i++ ) 
+            {
+                //Initiate two dice
+                int dice1 = 0;
+                int dice2 = 0;
+
+                //Choose a random number between 1-6 for each die
+                dice1 = r.Next(1,7);
+                dice2 = r.Next(1, 7);
+
+                //Adds sum of dice
+                int dicesum = dice2 + dice1;
+
+                //Stores counts of dice sum 
+                combos[(dicesum - 2)]++;
+            }
+            return combos;
+        }
+    }
+}
